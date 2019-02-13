2e95e45449436b40bf98d9fa1aa1a6192156fbc9
jrkinouani <kinouani@gmail.com>
2019-02-13 16:14:50 +0400

    fix pb root admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
+  devise_for :admins , path_names:{
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'signup',
+  }
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :admins
   root :to => 'pages#welcome'
   get "/about" => "pages#about"
   get "/comment-ca-marche" => "pages#comment"
