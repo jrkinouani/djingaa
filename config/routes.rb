8176baef8ca58f429f0fa59af14cb7bed711b806
jrkinouani <kinouani@gmail.com>
2019-04-14 00:44:19 +0400

    aaa

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  devise_for :admins , path_names:{
-    sign_in: 'login',
-    sign_out: 'logout',
-    sign_up: 'signup',
-  }
+  devise_for :users, path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      sign_up: 'signup',
+      :controllers => {registrations:  "users/registrations"}
+    }
+
+    devise_for :admin, path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      sign_up: 'signup',},
+      :controllers => {registrations:  "admins/registrations"}
+
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   resources :estimates
   root :to => 'pages#welcome'
   get "/about" => "pages#about"
