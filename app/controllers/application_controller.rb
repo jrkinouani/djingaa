bf9626b492e746580611cdb31f4f0a9d4040c73e
jrkinouani <kinouani@gmail.com>
2019-06-26 14:58:37 +0400

    add first_name and last_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
  protected
 
  def configure_permitted_parameters
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :phone])
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :lastname, :phone])
   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :lastname, :phone])
  end
 end
